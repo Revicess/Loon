e6a26865527cb09295452044608366504329da13
Lime <59417283+LimeAroma@users.noreply.github.com>
2022-08-09 17:58:43 +0800

    Update Bili_Auto_Regions.js

--- Script/Bili_Auto_Regions.js
+++ Script/Bili_Auto_Regions.js
@@ -1,6 +1,6 @@
 #修改
 [Script]
-http-response ^https:\/\/ap(p|i)\.bili(bili|api)\.(com|net)\/x\/resource\/tab\/v2\?access_key script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bili_Auto_Regions.js, requires-body=true, tag=bili自动地区
+http-response ^https:\/\/ap(p|i)\.bili(bili|api)\.(com|net)\/x\/resource\/ script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bili_Auto_Regions.js, requires-body=true, tag=bili自动地区
 
 #可选, 适用于搜索指定地区的番剧
 http-request ^https:\/\/ap(p|i)\.bili(bili|api)\.(com|net)\/x\/v2\/search(\/type|)\? script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bili_Auto_Regions.js, requires-body=true, tag=bili自动地区(搜索)
