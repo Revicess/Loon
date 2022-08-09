28717551640d5635c8d0ba8bdde1297a6bd70385
Lime <59417283+LimeAroma@users.noreply.github.com>
2022-08-09 16:58:13 +0800

    Create Bili_Auto_Regions.js

---
+++ Script/Bili_Auto_Regions.js
@@ -0,0 +1,9 @@
+#旧版本：2022.01.26
+[Script]
+http-response ^https:\/\/ap(p|i)\.bili(bili|api)\.(com|net)\/(pgc\/view\/v\d\/app\/season|x\/v\d\/search\/defaultwords)\?access_key script-path=https://raw.githubusercontent.com/NobyDa/Script/017919d9bc3ce4b5a9ad01f66f00bfe7d63c0f9c/Surge/JS/Bili_Auto_Regions.js, requires-body=true, tag=bili自动地区
+
+#可选, 适用于搜索指定地区的番剧
+http-request ^https:\/\/ap(p|i)\.bili(bili|api)\.(com|net)\/x\/v\d\/search(\/type)?\?.+?%20(%E6%B8%AF|%E5%8F%B0|%E4%B8%AD)& script-path=https://raw.githubusercontent.com/NobyDa/Script/017919d9bc3ce4b5a9ad01f66f00bfe7d63c0f9c/Surge/JS/Bili_Auto_Regions.js, requires-body=true, tag=bili自动地区(搜索)
+
+[Mitm]
+hostname = ap?.bilibili.com, ap?.biliapi.net
