516c40315c6f95b50158faa3fa3f5e0ae3a5c20d
Lime <59417283+LimeAroma@users.noreply.github.com>
2022-08-27 22:00:10 +0800

    Update B612.js

--- Script/B612.js
+++ Script/B612.js
@@ -2,4 +2,4 @@
 http-response ^https?:\/\/user-kaji-api\.b612kaji\.com\/v1\/purchase\/subscription\/subscriber\/status script-path=https://github.com/ddgksf2013/Cuttlefish/raw/master/Crack/612.js, requires-body=true, tag=B612会员
 
 [Mitm]
-hostname = *.b612kaji.com
+hostname = api.b612kaji.com
