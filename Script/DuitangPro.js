857fe721e7f9755f708ee96732a699114f7fd00b
Lime <59417283+LimeAroma@users.noreply.github.com>
2022-11-09 23:00:19 +0800

    Update DuitangPro.js

--- Script/DuitangPro.js
+++ Script/DuitangPro.js
@@ -1,4 +1,7 @@
-
+/*
+[Script]
+http-response ^https?:\/\/api\.duitang\.com\/napi\/vienna\/graphic\/template script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bili_Auto_Regions.js, requires-body=true, tag=bili自动地区
+*/
 
 var body = $response.body.replace(/vipType":"SVIP/g, 'vipType":"NONE');
 $done({ body });
