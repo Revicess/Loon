4e1bce87757197dd9b1b95f66d40dff73e825fea
Lime <59417283+LimeAroma@users.noreply.github.com>
2023-03-11 22:06:24 +0800

    Delete Bili_Auto_Regions.js

--- Script/Bili_Auto_Regions.js
+++
@@ -1,9 +0,0 @@
-#修改
-[Script]
-http-response ^https:\/\/ap(p|i)\.bili(bili|api)\.(com|net)\/x\/resource\/ script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bili_Auto_Regions.js, requires-body=true, tag=bili自动地区
-
-#可选, 适用于搜索指定地区的番剧
-http-request ^https:\/\/ap(p|i)\.bili(bili|api)\.(com|net)\/x\/v2\/search(\/type|)\? script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bili_Auto_Regions.js, requires-body=true, tag=bili自动地区(搜索)
-
-[Mitm]
-hostname = ap?.bili*i.com, ap?.bili*i.net
